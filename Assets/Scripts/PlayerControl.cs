f3f1b3771b5ed12afa888bee249fa1d2ba6cfe30
krpopp <kp1640@nyu.edu>
2023-03-15 16:02:59 -0400

    FPS input

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,18 +7,37 @@ public class PlayerControl : MonoBehaviour
 
     public float speed;
 
-    public float maxRotation;
-    public float minRotation;
+    public float upRotation;
+    public float downRotation;
+
+    CharacterController characterControl;
+    public Transform playerCam;
+
+    Vector3 vel;
+
+    public float lookSensitivity;
+
+    float xRotation = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterControl = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Rotate(0, Input.GetAxis("Mouse X") * lookSensitivity, 0);
+        xRotation -= Input.GetAxis("Mouse Y") * lookSensitivity;
+        xRotation = Mathf.Clamp(xRotation, -upRotation, downRotation);
+        playerCam.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+
+        vel.z = Input.GetAxis("Vertical") * speed;
+        vel.x = Input.GetAxis("Horizontal") * speed;
+
+        vel = transform.TransformDirection(vel);
+        characterControl.Move(vel * Time.deltaTime);
     }
 }
